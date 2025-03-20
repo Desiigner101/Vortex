3a33cd31ed9e3f47a254bfc0a8af5ab26840743e
Daniel Luis P. Garcia <danlugarcia221@gmail.com>
2025-03-20 17:26:41 +0800

    daniTheWowerz

---
+++ core/src/main/java/com/vortex/CharacterStats/Character_BattleStats.java
@@ -0,0 +1,13 @@
+package com.vortex.CharacterStats;
+
+public interface Character_BattleStats {
+    public int getHP();
+    public int getBasicAttackDamage();
+    public int getSkillDamage();
+    public int getSkillCost();
+    public int getUltimateDamage();
+    public int getUltCooldown();
+    public String getBasicAtkImage();
+    public String getSkillImage();
+    public String getUltImage();
+}
--- core/src/main/java/com/vortex/CharacterStats/Character_Umbra.java
+++ core/src/main/java/com/vortex/CharacterStats/Character_Umbra.java
@@ -1,7 +1,49 @@
 package com.vortex.CharacterStats;
 
-public class Character_Umbra {
+public class Character_Umbra implements Character_BattleStats{
     private int HP = 800;
-    private int basicAttackDamage = 40;
-
+    private int basicAttackDamage = 80;
+    private int skillDamage = 230;
+    private int skillCost = 3;
+    private int ultimateDamage = 300;
+    private int ultCooldown = 9; // turns
+    private String basicAtkImage = "Umbra_ShadowStrike.png";
+    private String skillImage = "Umbra_DistractingIllusions.png";
+    private String ultImage = "Umbra_VeilOfShadows.png";
+    @Override
+    public int getHP(){
+        return HP;
+    }
+    @Override
+    public int getBasicAttackDamage(){
+        return basicAttackDamage;
+    }
+    @Override
+    public int getSkillDamage(){
+        return skillDamage;
+    }
+    @Override
+    public int getSkillCost(){
+        return skillCost;
+    }
+    @Override
+    public int getUltimateDamage(){
+        return ultimateDamage;
+    }
+    @Override
+    public int getUltCooldown(){
+        return ultCooldown;
+    }
+    @Override
+    public String getBasicAtkImage(){
+        return basicAtkImage;
+    }
+    @Override
+    public String getSkillImage(){
+        return skillImage;
+    }
+    @Override
+    public String getUltImage(){
+        return ultImage;
+    }
 }
